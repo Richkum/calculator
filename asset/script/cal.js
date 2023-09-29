c1ea97441ae36e264e5ae6717385d97dfca20211
Richkum <kumatum2@gmail.com>
2023-09-29 09:19:55 +0100

    finish

--- asset/script/cal.js
+++ asset/script/cal.js
@@ -1,81 +1,15 @@
-const numbers = document.querySelectorAll('.numb');
-const display = document.querySelector('.display textarea');
-const caltn = document.querySelector('.caltn');
-const equals = document.querySelector('.equals');
-const clear = document.querySelectorAll('.clear');
-const negpos = document.querySelector('.negpos');
-const percent = document.querySelector('.percent');
+const numbers = document.querySelectorAll('.numb')
+const display = document.querySelector('.display textarea')
 
-
-let firstValue = "";
-let isFirstValue = false;
-let secondValue = "";
-let isSecondValue = false;
-let sign = "";
-let displayValue = 0;
-
-for(let i = 0; i <numbers.length; i++) {
-  numbers[i].addEventListener('click', (e) => {
-    let atr = e.target.getAttribute('value');
-    if(isFirstValue  === false) {
-      getFirstValue(atr);
+let string = ''
+const arr = Array.from(numbers)
+arr.forEach(numb => {
+  numb.addEventListener('click', (e) => {
+    if (e.target.innerHTML === '=') {
+      // string = eval(string)
+      // display.innerHTML = string
     }
-    if(isSecondValue === false) {
-      getSecondValue(atr);
-    }
+    string += e.target.innerHTML
+    display.innerHTML = string
   })
-}
-
-function getFirstValue(el) {
-  display.innerHTML = "";
-  firstValue += el;
-  display.innerHTML = firstValue;
-  firstValue = +firstValue;
-}
-
-function getSecondValue(el) {
-  if(firstValue != "" && sign != "") {
-    secondValue += el;
-    display.innerHTML = secondValue;
-    secondValue = +secondValue;
-  }
-}
-
-function getCaltn() {
-  for(let i = 0; i < caltn.length; i++) {
-    caltn[i].addEventListener('click', (e) => {
-      sign = e.target.getAttribute('value');
-    })
-  }
-}
-getCaltn();
-
-equals.addEventListener('click', () => {
-  display.innerHTML = "";
-  if(sign === '+') {
-    displayValue = firstValue + secondValue;
-  }else if(sign === '-') {
-    displayValue = firstValue - secondValue;
-  }else if(sign === '*') {
-    displayValue = firstValue * secondValue;
-  }else if(sign === '/') {
-    displayValue = firstValue / secondValue;
-  }
-  display.innerHTML = displayValue;
-  firstValue = displayValue;
-  secondValue = "";
-
-  checkDisplaytLength();
-
 })
-
-function checktLength() {
-  displayValue = JSON.stringify(resultsValue);
-
-  if(resultsValue.length >= 8) {
-    resultsValue = JSON.parse(resultsValue);
-    results.innerHTML = resultsValue.toFixed(5);
-  }
-}
-
-
